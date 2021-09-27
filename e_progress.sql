29f8bf45fcec7e66c07cafc344adcbe3fca1f597
rshnewarray <rsh.newarray@gmail.com>
2021-09-27 15:24:32 +0800

    db

--- e_progress.sql
+++ e_progress.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Waktu pembuatan: 27 Sep 2021 pada 09.26
--- Versi server: 10.3.16-MariaDB
--- Versi PHP: 7.2.19
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Sep 2021 pada 09.24
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -41,7 +40,24 @@ CREATE TABLE `tb_file` (
 --
 
 INSERT INTO `tb_file` (`id_file`, `id_project`, `nama_file`, `size`, `file`) VALUES
-(1, 1, 'Screenshot (11).png', '1.454', '1632703905_960b9a84e958f5772942.png');
+(1, 1, 'Screenshot (11).png', '1.454', '1632703905_960b9a84e958f5772942.png'),
+(3, 2, 'datasheet_manual.xlsx', '0.022', '1632709906_874d09b5030ca0739485.xlsx'),
+(4, 2, 'datasheet_manual.csv', '0.029', '1632709906_e8865c843fc154091094.csv'),
+(5, 2, '', '0.000', '1632709942_1347efa8898f9a853081');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_member`
+--
+
+CREATE TABLE `tb_member` (
+  `id_member` varchar(5) NOT NULL,
+  `nama_member` varchar(50) NOT NULL,
+  `divisi` varchar(50) NOT NULL,
+  `foto` text NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -71,7 +87,7 @@ CREATE TABLE `tb_project` (
 --
 
 INSERT INTO `tb_project` (`id_project`, `pic`, `durasi`, `periode`, `project`, `desc_project`, `project_progress`, `status`, `tgl_sit`, `tgl_uat`, `tgl_to`, `os`, `message_h2h`, `time_stamp`) VALUES
-(1, 'test', '1 bulan', 'April 2021', 'test', 'test project', 50, '0', '2020-02-20', '2020-02-20', '2020-02-20', 'linux', '-', '0000-00-00 00:00:00');
+(2, '123', '123', '123', '123', 'asfsaasfsasfsa', 25, '0', '2020-02-02', '2020-02-02', '2020-02-02', '12', '12', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -82,6 +98,12 @@ INSERT INTO `tb_project` (`id_project`, `pic`, `durasi`, `periode`, `project`, `
 --
 ALTER TABLE `tb_file`
   ADD PRIMARY KEY (`id_file`);
+
+--
+-- Indeks untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  ADD PRIMARY KEY (`id_member`);
 
 --
 -- Indeks untuk tabel `tb_project`
@@ -97,13 +119,13 @@ ALTER TABLE `tb_project`
 -- AUTO_INCREMENT untuk tabel `tb_file`
 --
 ALTER TABLE `tb_file`
-  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_project`
 --
 ALTER TABLE `tb_project`
-  MODIFY `id_project` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_project` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
